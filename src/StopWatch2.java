ee24cde984ab49a4db8315f92070f9019bda4b52
Irpan Budiana <irpan.budiana@gmail.com>
2017-05-29 13:24:00 +0700

    running stopwatch sederhana

---
+++ src/StopWatch2.java
@@ -0,0 +1,54 @@
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author irpan
+ */
+public class StopWatch2 extends JFrame implements ActionListener {
+    JLabel lbCounter = new JLabel();
+    Timer timer = new Timer(100, this);
+
+    int detik = 0;
+    int menit = 0;
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == timer) {
+            if (detik < 59) {
+                detik++;
+            } else {
+                detik = 0;
+                menit++;
+            }
+            lbCounter.setText(menit + ":" + detik);
+        }
+    }
+
+    public StopWatch2() {
+        this.setSize(300, 200);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        lbCounter.setText("0:0");
+        lbCounter.setBounds(10, 40, 270, 80);
+        lbCounter.setFont(new Font("Arial", 0, 100));
+        lbCounter.setHorizontalAlignment(JLabel.CENTER);
+        this.add(lbCounter);
+
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        new StopWatch2();
+    }
+}
