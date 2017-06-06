b312c36118bfe3d058f4e7299769a1972240e918
Irpan Budiana <irpan.budiana@gmail.com>
2017-06-06 10:52:24 +0700

    Latihan bab 1

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author irpan
+ */
+public class Hello {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to Java World.");
+    }
+}
